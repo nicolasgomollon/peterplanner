0de7b51943e0dc0b0777456c33cb70e45d6f7b9f
Nicolas Gomollon <nicolas@gomollon.me>
2017-02-12 10:28:32 -0800

    Refactored XML parsing code.

--- parsers/xml-parser.go
+++ parsers/xml-parser.go
@@ -9,38 +9,47 @@ import (
 
 /* DegreeWorks XML Parser */
 
-func Parse(doc *etree.Document) (map[string]types.Course, map[string]types.Block, map[string]bool) {
+func Parse(doc *etree.Document) (map[string]types.Course, map[string]bool, []types.Block, map[string]bool) {
 	courses := make(map[string]types.Course, 0)
-	blocks := make(map[string]types.Block, 0)
+	taken := make(map[string]bool, 0)
+	blocks := make([]types.Block, 0)
 	prereqDepts := make(map[string]bool, 0)
 	root := doc.SelectElement("Report").SelectElement("Audit")
 	for _, block := range root.SelectElements("Block") {
 		reqType := block.SelectAttrValue("Req_type", "unknown")
-		if (reqType == "MAJOR") || (reqType == "MINOR") {
-			blocks[reqType] = parseBlock(block, &courses, &prereqDepts)
+		if (reqType == "PROGRAM") {
+			parseProgram(block, &courses, &taken, &blocks, &prereqDepts)
+		} else if (reqType == "MAJOR") || (reqType == "MINOR") {
+			parseBlock(block, &courses, &taken, &blocks, &prereqDepts)
 		}
 	}
-	return courses, blocks, prereqDepts
+	return courses, taken, blocks, prereqDepts
 }
 
-func parseBlock(block *etree.Element, courses *map[string]types.Course, prereqDepts *map[string]bool) types.Block {
-	taken := make(map[string]bool, 0)
+func parseProgram(block *etree.Element, courses *map[string]types.Course, taken *map[string]bool, blocks *[]types.Block, prereqDepts *map[string]bool) {
+	
+}
+
+func parseBlock(block *etree.Element, courses *map[string]types.Course, taken *map[string]bool, blocks *[]types.Block, prereqDepts *map[string]bool) {
 	requirements := make([]types.Requirement, 0)
 	for _, rule := range block.SelectElements("Rule") {
 		rules := rule.SelectElements("Rule")
 		if len(rules) > 0 {
 			// TODO: Need to determine how to handle this case, because only certain rule blocks may be necessary.
 			for _, r := range rules {
-				parseRule(r, courses, prereqDepts, &taken, &requirements)
+				parseRule(r, courses, taken, prereqDepts, &requirements)
 			}
 		} else {
-			parseRule(rule, courses, prereqDepts, &taken, &requirements)
+			parseRule(rule, courses, taken, prereqDepts, &requirements)
 		}
 	}
-	return types.Block{Taken: taken, Requirements: requirements}
+	reqType := block.SelectAttrValue("Req_type", "UNKNOWN")
+	title := block.SelectAttrValue("Title", "Untitled")
+	theBlock := types.Block{ReqType: reqType, Title: title, Requirements: requirements}
+	*blocks = append(*blocks, theBlock)
 }
 
-func parseRule(rule *etree.Element, courses *map[string]types.Course, prereqDepts *map[string]bool, taken *map[string]bool, requirements *[]types.Requirement) {
+func parseRule(rule *etree.Element, courses *map[string]types.Course, taken *map[string]bool, prereqDepts *map[string]bool, requirements *[]types.Requirement) {
 	// Remaining Classes
 	advice := rule.SelectElement("Advice")
 	if advice != nil {
