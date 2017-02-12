3144320df1a0b4b11f66046029cfd58e50438de0
Nicolas Gomollon <nicolas@gomollon.me>
2017-02-12 10:16:39 -0800

    Added ability to fetch XML file from DegreeWorks.

---
+++ helpers/server.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+)
+
+/* Exported Constants & Variables */
+const Timeout = time.Duration(210 * time.Second)
+
+/* Unexported Variables */
+var client = http.Client{Timeout: Timeout}
+
+func Post(url string, cookie string, data string) (statusCode int, responseBody string, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = r.(error)
+		}
+	}()
+
+	statusCode = 0
+	request, err := http.NewRequest("POST", url, strings.NewReader(data))
+	if err != nil {
+		panic(err)
+	}
+
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Set("Cookie", cookie)
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	statusCode = response.StatusCode
+
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	return statusCode, string(contents), nil
+}
--- main.go
+++ main.go
@@ -1,15 +1,67 @@
 package main
 
-import "fmt"
-import "github.com/beevik/etree"
-import "strconv"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/beevik/etree"
+	"github.com/nicolasgomollon/peterplanner/helpers"
+	"golang.org/x/net/html/charset"
+	"net/http"
+	"regexp"
+	"strconv"
+)
 
-func main() {
+const DegreeWorksURL = "https://www.reg.uci.edu/dgw/IRISLink.cgi"
+// const FullBody = "SERVICE=SCRIPTER&REPORT=WEB31&SCRIPT=SD2GETAUD%26ContentType%3Dxml&USERID=33897573&USERCLASS=STU&BROWSER=NOT-NAV4&ACTION=REVAUDIT&AUDITTYPE=&DEGREETERM=ACTV&INTNOTES=&INPROGRESS=N&CUTOFFTERM=ACTV&REFRESHBRDG=N&AUDITID=&JSERRORCALL=SetError&NOTENUM=&NOTETEXT=&NOTEMODE=&PENDING=&INTERNAL=&RELOADSEP=TRUE&PRELOADEDPLAN=&ContentType=xml&STUID=33897573&SCHOOL=U&STUSCH=U&DEGREE=BS&STUDEG=BS&STUDEGLIT=B.S.&STUDI=&STULVL=Senior&STUMAJLIT=Software+Engineering&STUCATYEAR=&CLASSES=&DEBUG=OFF"
+
+func fetchStudentID(cookie string) (string, error) {
+	body := "SERVICE=SCRIPTER&SCRIPT=SD2STUCON"
+	statusCode, responseHTML, err := server.Post(DegreeWorksURL, cookie, body)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("ERROR: Unable to fetch DegreeWorks HTML file. `%v`.", err.Error()))
+	} else if statusCode != http.StatusOK {
+		return "", errors.New(fmt.Sprintf("ERROR: Unable to fetch DegreeWorks HTML file. HTTP status code: %v.", statusCode))
+	}
+	r, _ := regexp.Compile(`(?s)<input type="hidden" name="STUID" value="(\d*)">`)
+	matches := r.FindStringSubmatch(responseHTML)
+	if len(matches) == 0 {
+		return "", errors.New(fmt.Sprintf("ERROR: Unable to fetch DegreeWorks HTML file. Invalid cookies."))
+	}
+	studentID := r.FindStringSubmatch(responseHTML)[1]
+	return studentID, nil
+}
+
+func fetchXML(studentID string, cookie string) (string, error) {
+	body := fmt.Sprintf("SERVICE=SCRIPTER&REPORT=WEB31&SCRIPT=SD2GETAUD%%26ContentType%%3Dxml&ACTION=REVAUDIT&ContentType=xml&STUID=%v&DEBUG=OFF", studentID)
+	statusCode, responseXML, err := server.Post(DegreeWorksURL, cookie, body)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("ERROR: Unable to fetch DegreeWorks XML file. `%v`.", err.Error()))
+	} else if statusCode != http.StatusOK {
+		return "", errors.New(fmt.Sprintf("ERROR: Unable to fetch DegreeWorks XML file. HTTP status code: %v.", statusCode))
+	}
+	return responseXML, nil
+}
+
+func readFromString(contentsXML string) {
 	doc := etree.NewDocument()
-	if err := doc.ReadFromFile("DGW_Report.xsl"); err != nil {
+	doc.ReadSettings.CharsetReader = charset.NewReaderLabel
+	if err := doc.ReadFromString(contentsXML); err != nil {
 		panic(err)
 	}
-	
+	parse(doc)
+}
+
+func readFromFile(fileName string) {
+	doc := etree.NewDocument()
+	doc.ReadSettings.CharsetReader = charset.NewReaderLabel
+	if err := doc.ReadFromFile(fileName); err != nil {
+		panic(err)
+	}
+	parse(doc)
+}
+
+func parse(doc *etree.Document) {
 	root := doc.SelectElement("Report").SelectElement("Audit")
 	for _, block := range root.SelectElements("Block") {
 		reqType := block.SelectAttrValue("Req_type", "unknown")
@@ -47,5 +99,29 @@ func main() {
 		// for _, attr := range block.Attr {
 		// 	fmt.Printf("  ATTR: %s=%s\n", attr.Key, attr.Value)
 		// }
+	}
+}
+
+func main() {
+	studentIDptr := flag.String("studentID", "", "Fetch DegreeWorks XML file for the specified student ID.")
+	cookiePtr := flag.String("cookie", "", "Fetch DegreeWorks XML file using specified cookies.")
+	flag.Parse()
+	
+	if len(*cookiePtr) > 0 {
+		if len(*studentIDptr) == 0 {
+			studentID, err := fetchStudentID(*cookiePtr)
+			if err != nil {
+				panic(err)
+			}
+			*studentIDptr = studentID
+		}
+		responseXML, err := fetchXML(*studentIDptr, *cookiePtr)
+		if err != nil {
+			panic(err)
+		}
+		readFromString(responseXML)
+	} else {
+		fmt.Println("No flags were specified. Use `-h` or `--help` flags to get help.")
+		// readFromFile("DGW_Report.xsl")
 	}
 }
